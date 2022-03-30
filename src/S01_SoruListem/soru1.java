10ccdc898e943ed69b286d7cd5dc013157601023
Cm2727 <102066115+Cm2727@users.noreply.github.com>
2022-03-30 12:01:21 +0200

    Update soru1.java

--- src/S01_SoruListem/soru1.java
+++ src/S01_SoruListem/soru1.java
@@ -4,6 +4,7 @@ public class soru1 {
     public static void main(String[] args) {
 
         System.out.println("Suan github anlammaya calisiyorum.");
+        System.out.println("Github dan gonderiyorum.");
 
     }
 }
