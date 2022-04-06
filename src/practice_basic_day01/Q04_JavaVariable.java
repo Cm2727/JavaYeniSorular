4e9433acac851b5e5df6277a57e7c0259c15344c
Cm2727 <cuma.ozturkmen@gmail.com>
2022-04-06 05:12:50 +0200

    06.04.2022

---
+++ src/practice_basic_day01/Q04_JavaVariable.java
@@ -0,0 +1,52 @@
+package practice_basic_day01;
+
+public class Q04_JavaVariable {
+    public static void main(String[] args) {
+
+        // Initializing Variable  //variable baslangic degeri verme
+        int age=32;
+        int temp=52;
+
+        System.out.println(age);
+        System.out.println("temp = " +temp );
+
+
+        // To copy the variable's value
+
+        int myAge=33;
+        int hisAge=myAge;
+
+
+
+        // you can declare multiple variables in the same line
+        int year=2022, month=3, day=4;
+
+
+
+        // updating a variable
+        year=2032;
+
+
+
+        //Bir degisken tanimlayin : x
+        //Baska bir degiskeni initialize edin : y
+        //x degiskenini initialize edin
+        //y degiskenini yeni bir degerle guncelleyin
+
+        int x;
+        int y=2000;
+        
+        x=234;
+        y=125;
+
+        System.out.println("x= "+ x);
+        System.out.println("y = " + y);
+
+
+
+
+
+
+
+    }
+}
---
+++ src/practive_basic/day1Soru2.java
@@ -0,0 +1,25 @@
+package practive_basic;
+
+public class day1Soru2 {
+
+     /*
+  println() ve print() arasinda tek fark var;
+  println satir atliyor,
+  print ise imlecin ayni satirda kalmasini sagliyor.
+   */
+     public static void main(String[] args) {
+         System.out.println("******println*****");
+         System.out.println("Weocome");
+         System.out.println("to");
+         System.out.println("Java");
+         System.out.println("Class");
+
+
+         System.out.println("******print******");
+         System.out.print("Welcome");
+         System.out.print(" to");
+         System.out.print(" Java");
+         System.out.print(" Class");
+
+     }
+}
